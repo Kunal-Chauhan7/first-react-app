cd3d309620fcbaed8b4fbb15f3a3986535a9fe44
Kunal Chauhan <kunalchauhanlion@gmail.com>
2023-08-09 02:00:16 +0530

    made use of useState Hook

--- src/components/FunctionalComponent.jsx
+++ src/components/FunctionalComponent.jsx
@@ -1,4 +1,5 @@
 import React from 'react'; 
+import {useState} from 'react';// importing the useState Hook
 
 //function FunctionalComponent (){
 //    return(
@@ -9,9 +10,15 @@ import React from 'react';
 //}
 
 const FunctionalComponent = ()=>{
+    // Here all the javaScript logic will be written before the return statement
+
+    const[count,setCount] = useState(0);
+
     return(
         <div>
             <p>This is Functional Component</p>
+            <button onClick={() => setCount(count +1 ) }>Click here to add one</button>
+            <h1>{count}</h1>
         </div>
     );
 };
