ec9e2b86aed35a955ea207e989c5d8037bde56b5
Kunal Chauhan <kunalchauhanlion@gmail.com>
2023-08-09 16:50:36 +0530

    used Props

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import './App.css';
 import ClassComponent from './components/ClassComponent'; // loading the component
 import FunctionalComponent from './components/FunctionalComponent';
-
+import { useState } from 'react';
 
 function App() {
+
+  const [name,Setname] = useState("Kunal");
+  const dadname = "Pardeep Kumar Chauhan"
   return (
   <div className="App">
     <h1>Class Component</h1>
     <ClassComponent/>
     <br/>
     <h1>Functional Component</h1>
-    <FunctionalComponent/>
+    <FunctionalComponent  name={name}  ChangeName = {Setname} age = {20} FatherName = {dadname} Mothername = "Rekha devi"/>
   </div>
+  //sending props to functional component 
   // we are using the component in the acnhor tag 
   // we can also use the component like <ClassComponent></ClassComponent> 
   );
--- src/components/FunctionalComponent.jsx
+++ src/components/FunctionalComponent.jsx
@@ -9,7 +9,7 @@ import {useState} from 'react';// importing the useState Hook
 //    );
 //}
 
-const FunctionalComponent = ()=>{
+const FunctionalComponent = (props)=>{ //accesing Props using props
     // Here all the javaScript logic will be written before the return statement
 
     const[count,setCount] = useState(0);
@@ -19,6 +19,8 @@ const FunctionalComponent = ()=>{
             <p>This is Functional Component</p>
             <button onClick={() => setCount(count +1 ) }>Click here to add one</button>
             <h1>{count}</h1>
+            <h2>My name is {props.name} and I am {props.age} old and my Father name is {props.FatherName} and mother's name is {props.Mothername}</h2>
+            <button onClick={() => {props.ChangeName("Kanu")}}>Change to Nick Name</button>
         </div>
     );
 };
