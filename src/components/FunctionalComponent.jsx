cec4858e4c83ace868e09f9b889eae52782d8eb4
Kunal Chauhan <kunalchauhanlion@gmail.com>
2023-08-09 16:58:52 +0530

    made a custom name

--- src/components/FunctionalComponent.jsx
+++ src/components/FunctionalComponent.jsx
@@ -12,7 +12,8 @@ import {useState} from 'react';// importing the useState Hook
 const FunctionalComponent = (props)=>{ //accesing Props using props
     // Here all the javaScript logic will be written before the return statement
 
-    const[count,setCount] = useState(0);
+    const [count,setCount] = useState(0);
+    const [changedname , Changename] = useState("");
 
     return(
         <div>
@@ -20,7 +21,8 @@ const FunctionalComponent = (props)=>{ //accesing Props using props
             <button onClick={() => setCount(count +1 ) }>Click here to add one</button>
             <h1>{count}</h1>
             <h2>My name is {props.name} and I am {props.age} old and my Father name is {props.FatherName} and mother's name is {props.Mothername}</h2>
-            <button onClick={() => {props.ChangeName("Kanu")}}>Change to Nick Name</button>
+            <input onChange={(e)=>{Changename(e.target.value)}}></input>
+            <button onClick={() => {props.ChangeName(changedname)}}>Change to Nick Name</button>
         </div>
     );
 };
