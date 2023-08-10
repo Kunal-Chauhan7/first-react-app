d3317322e28bb48d48c358eaaab8ccfae93de89a
Kunal Chauhan <kunalchauhanlion@gmail.com>
2023-08-11 01:34:50 +0530

    made the routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
 import './App.css';
-import ClassComponent from './components/ClassComponent'; // loading the component
-import FunctionalComponent from './components/FunctionalComponent';
+import ClassComponent from './pages/ClassComponent'; // loading the component
+import FunctionalComponent from './pages/FunctionalComponent';
 import { useState } from 'react';
+import { Route , Routes } from 'react-router-dom';
 
 function App() {
 
   const [name,Setname] = useState("Kunal");
   const dadname = "Pardeep Kumar Chauhan"
   return (
-  <div className="App">
-    <h1>Class Component</h1>
-    <ClassComponent/>
-    <br/>
-    <h1>Functional Component</h1>
-    <FunctionalComponent  name={name}  ChangeName = {Setname} age = {20} FatherName = {dadname} Mothername = "Rekha devi"/>
-  </div>
+    <Routes>
+      <Route path='/' element = {<ClassComponent />} />
+      <Route path='/functionalComponent' element = {<FunctionalComponent name={name}  ChangeName = {Setname} age = {20} FatherName = {dadname} Mothername = "Rekha devi"/>} />
+    </Routes>
   //sending props to functional component 
   // we are using the component in the acnhor tag 
   // we can also use the component like <ClassComponent></ClassComponent> 
   );
 }
+
+//<div className="App">
+//    <h1>Class Component</h1>
+//    <ClassComponent/>
+//    <br/>
+//    <h1>Functional Component</h1>
+//    <FunctionalComponent  name={name}  ChangeName = {Setname} age = {20} FatherName = {dadname} Mothername = "Rekha devi"/>
+//  </div>
+
+
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,13 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App'; //loding the app file
+import { BrowserRouter } from 'react-router-dom'; //creating a routeBoundray around root
 
 const root = ReactDOM.createRoot(document.getElementById('root')); 
 //making a root  of the reat app where the app will start
 root.render(
   //this strict mode means run on the stable version of react 
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
   // <App /> is laoding the app
 );
